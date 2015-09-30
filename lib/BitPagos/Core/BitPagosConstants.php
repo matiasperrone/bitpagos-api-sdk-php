2ac8c2a0d83cace5e43d037e5640949199a73c7a
Matias Perrone <matias.perrone@gmail.com>
2015-09-30 17:16:22 -0300

    Version update

--- lib/BitPagos/Core/BitPagosConstants.php
+++ lib/BitPagos/Core/BitPagosConstants.php
@@ -11,12 +11,12 @@ namespace BitPagos\Core;
 class BitPagosConstants
 {
 	const SDK_NAME = 'bitpagos-api-sdk-php';
-	const SDK_VERSION = '0.2.2';
-	
+	const SDK_VERSION = '1.0.3 RC2';
+
 	// AUTH
 	const AUTH_DATA = 'client_id=[CLIEND_ID]&redirect_uri=[CALLBACK_URI]&response_type=code&scope=read+payments';
 	const HEADERS = "Authorization: OAuth [TOKEN]";
-	
+
 	// SANDBOX
 	const REST_SANDBOX_ENDPOINT = 'https://www.bitpagos.net';
 	// PRODUCCION
